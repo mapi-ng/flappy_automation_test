924c5da69f3870cfd89ee2a1e28b5cd31727607e
Marcin Pilch <marcinpilch4@gmail.com>
2021-07-28 19:54:20 +0200

    Refactor to use a class

--- flappy_automation_code/src/flappy_automation_code.cpp
+++ flappy_automation_code/src/flappy_automation_code.cpp
@@ -1,40 +1,50 @@
-#include "ros/ros.h"
-#include "flappy_automation_code/flappy_automation_code.hpp"
+#include <ros/ros.h>
 #include "sensor_msgs/LaserScan.h"
 #include "geometry_msgs/Vector3.h"
 
-void initNode()
+class BirdController
 {
-  //Initialization of nodehandle
-  nh_ = new ros::NodeHandle();
-  //Init publishers and subscribers
-  pub_acc_cmd = nh_->advertise<geometry_msgs::Vector3>("/flappy_acc",1);
-  sub_vel = nh_->subscribe<geometry_msgs::Vector3>("/flappy_vel", 1, velCallback);
-  sub_laser_scan = nh_->subscribe<sensor_msgs::LaserScan>("/flappy_laser_scan", 1, laserScanCallback);
-}
+  private:
+    //Publisher for acceleration command
+    ros::Publisher pub_acc_cmd_;
+    //Subscriber for velocity
+    ros::Subscriber sub_vel_;
+    //Subscriber for laser scan
+    ros::Subscriber sub_laser_scan_;
 
-void velCallback(const geometry_msgs::Vector3::ConstPtr& msg)
-{
-  // msg has the format of geometry_msgs::Vector3
-  // Example of publishing acceleration command on velocity velCallback
-  geometry_msgs::Vector3 acc_cmd;
+  public:
+    BirdController(ros::NodeHandle &nh)
+    {
+      pub_acc_cmd_ = nh.advertise<geometry_msgs::Vector3>("/flappy_acc",1);
+      sub_vel_ = nh.subscribe<geometry_msgs::Vector3>("/flappy_vel", 1, &BirdController::velCallback, this);
+      sub_laser_scan_ = nh.subscribe<sensor_msgs::LaserScan>("/flappy_laser_scan", 1, &BirdController::laserScanCallback, this);
+    }
 
-  acc_cmd.x = 0;
-  acc_cmd.y = 0;
-  pub_acc_cmd.publish(acc_cmd);
-}
+    void velCallback(const geometry_msgs::Vector3::ConstPtr& msg)
+    {
+      // msg has the format of geometry_msgs::Vector3
+      // Example of publishing acceleration command on velocity velCallback
+      geometry_msgs::Vector3 acc_cmd;
 
-void laserScanCallback(const sensor_msgs::LaserScan::ConstPtr& msg)
-{
-  //msg has the format of sensor_msgs::LaserScan
-  //print laser angle and range
-  ROS_INFO("Laser range: %f, angle: %f", msg->ranges[0], msg->angle_min);
-}
+      acc_cmd.x = 0;
+      acc_cmd.y = 0;
+      pub_acc_cmd_.publish(acc_cmd);
+    }
+
+    void laserScanCallback(const sensor_msgs::LaserScan::ConstPtr& msg)
+    {
+      //msg has the format of sensor_msgs::LaserScan
+      //print laser angle and range
+      ROS_INFO("Laser range: %f, angle: %f", msg->ranges[0], msg->angle_min);
+    }
+};
 
 int main(int argc, char **argv)
 {
-  ros::init(argc,argv,"flappy_automation_code");
-  initNode();
+  //Initialization of nodehandle
+  ros::init(argc, argv, "flappy_automation_code");
+  ros::NodeHandle nh;
+  BirdController bird_controller(nh);
 
   // Ros spin to prevent program from exiting
   ros::spin();
