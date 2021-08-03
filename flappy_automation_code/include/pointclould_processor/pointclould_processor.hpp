51b743ee8b320145f722ea27bc89d4c7b30c3a89
Marcin Pilch <marcinpilch4@gmail.com>
2021-08-03 12:23:32 +0200

    Cluster extraction

--- flappy_automation_code/include/pointclould_processor/pointclould_processor.hpp
+++ flappy_automation_code/include/pointclould_processor/pointclould_processor.hpp
@@ -1,14 +1,22 @@
 #include <ros/ros.h>
 #include <laser_assembler/AssembleScans2.h>
 #include <sensor_msgs/PointCloud2.h>
+#include <pcl/point_cloud.h>
+#include <pcl/point_types.h>
+#include <pcl_conversions/pcl_conversions.h>
 
 class PointCloudProcessor
 {
   private:
     bool is_started_ = false;
     bool publish_pointclouds_ = false;
-    float leaf_size_ = 0.5f;
-    ros::Rate rate_ = 4;
+    float leaf_size_ = 0.15f;
+
+    float cluster_tolerance_ = 0.6;
+    float min_cluster_size_ = 5;
+    float max_cluster_size_ = 50;
+
+    ros::Rate rate_ = 2;
 
     ros::ServiceClient laser_assembler_client_;
     //Publisher for assembled pointcloud2
@@ -17,6 +25,7 @@ class PointCloudProcessor
 
     std::unique_ptr<sensor_msgs::PointCloud2> requestPointCloud();
     void processPointClould(const sensor_msgs::PointCloud2& clould_msg);
+    void extractClusters(const pcl::PCLPointCloud2& cloud);
 
   public:
     PointCloudProcessor() = delete;
